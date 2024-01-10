3cb411ef8f5194f5e4de6ab42c01a178f0a46530
Zach Waugh <zwaugh@gmail.com>
2024-01-10 15:24:30 -0500

    Add additional target

--- Package.swift
+++ Package.swift
@@ -1,23 +1,16 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "swiftski",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swiftski",
-            targets: ["swiftski"]),
+        .library(name: "swiftski", targets: ["swiftski"]),
+        .library(name: "gifski", targets: ["gifski"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "swiftski"),
-        .testTarget(
-            name: "swiftskiTests",
-            dependencies: ["swiftski"]),
+        .target(name: "swiftski", dependencies: ["Gifski"]),
+        .testTarget(name: "swiftskiTests", dependencies: ["swiftski"]),
+        .binaryTarget(name: "Gifski", path: "Gifski.xcframework")
     ]
 )
